b51bce3367bb4b717aad4e7c1a3a4647d39c5102
Justin Carpenter <JustinCarpenter2020@Gmail.com>
2021-06-25 22:30:27 -0600

    random trending

--- B-Team-server/server/services/TrendingService.js
+++ B-Team-server/server/services/TrendingService.js
@@ -3,15 +3,19 @@ import { dbContext } from '../db/DbContext'
 class TrendingService {
   async getTrending() {
     const names = await this.getNames()
-    return names
+    const locations = ['Fast Eddies', 'the back of a wendys', 'a public park', 'the emmys', 'the nickelodeon kids choice awards', 'their grandmas house', 'an undisclosed location', 'in the back of ryan goslings car', 'a dennys parking lot']
+
+    const actions = [`${names[0]} got Mistaken for ${names[1]}`, `${names[0]} filed a cease and assist against ${names[1]}`, `${names[0]} landed a role as guard #3`, `${names[1]} was held captive by ${names[0]} at ${locations[Math.floor(Math.random() * (locations.length - 1))]}`, `${names[1]} was spotted mingling at ${locations[Math.floor(Math.random() * (locations.length - 1))]}`, `${names[0]} makes a public statement that they do live in a box near ${locations[Math.floor(Math.random() * (locations.length - 1))]}`]
+
+    return actions[Math.floor(Math.random() * (actions.length - 1))]
   }
 
   async getNames() {
     const names = []
     const count = await dbContext.Account.count()
-    const random = Math.floor(Math.random() * count)
+    const random = Math.floor(Math.random() * (count - 1))
     const name = await dbContext.Account.find({}).limit(2).skip(random)
-    name.forEach(n => names.push(n.name))
+    name.forEach(n => names.push(n.name.split('@')[0]))
     return names
   }
 }
