080eac7d68438ca216fcbf8989b8e63fcaa2426f
Justin <JustinCarpenter2020@gmail.com>
2021-10-28 11:40:14 -0600

    trending service refined

--- B-Team-server/server/services/TrendingService.js
+++ B-Team-server/server/services/TrendingService.js
@@ -7,31 +7,38 @@ class TrendingService {
       const count = await dbContext.Account.count()
       const random = Math.floor(Math.random() * (count - 1))
       const names = await dbContext.Account.find({}).limit(2).skip(random)
-      people.push(names[0], names[1])
+      people.push(...names)
     }
     return people
   }
 
   async getTrending() {
-    // const names = await this.getNames()
-    const locations = ['Fast Eddies', 'the back of a wendys', 'a public park', 'the emmys', 'the nickelodeon kids choice awards', 'their grandmas house', 'an undisclosed location', 'the waffleHouse', 'a dennys parking lot', 'a apple genius bar']
-
-    const output = []
+    const hashTags = []
     for (let i = 0; i <= 5; i++) {
-      const names = await this.getNames()
-      const actions = [`${names[0]} got Mistaken for ${names[1]}`, `${names[0]} filed a cease and assist against ${names[1]}`, `${names[0]} landed a role as guard #3`, `${names[1]} was held captive by ${names[0]} at ${locations[Math.floor(Math.random() * (locations.length - 1))]}`, `${names[1]} was spotted mingling at ${locations[Math.floor(Math.random() * (locations.length - 1))]}`, `${names[0]} makes a public statement that they do live in a box near ${locations[Math.floor(Math.random() * (locations.length - 1))]}`]
-      output.push(actions[Math.floor(Math.random() * (actions.length - 1))])
+      const name = await this.getName()
+      const statements = this.getJoinStatement()
+      hashTags.push(`${name} ${statements.statement} ${statements.type === 'user' ? await this.getName() : this.getLocation()}`)
     }
-    return output
+    return hashTags
   }
 
-  async getNames() {
-    const names = []
+  async getName() {
     const count = await dbContext.Account.count()
     const random = Math.floor(Math.random() * (count - 1))
-    const name = await dbContext.Account.find({}).limit(2).skip(random)
-    name.forEach(n => names.push(n.name.split('@')[0]))
-    return names
+    const account = await dbContext.Account.findOne({}).limit(1).skip(random)
+    return account.name.split('@')[0]
+  }
+
+  getLocation() {
+    const locations = ['Fast Eddies', 'wendys', 'public park', 'the Emmys', 'the nickelodeon kids choice awards', 'grandmas house', ' undisclosed location', 'waffleHouse', 'dennys parking lot', 'apple genius bar']
+    const random = Math.floor(Math.random() * (locations.length - 1))
+    return locations[random]
+  }
+
+  getJoinStatement() {
+    const statements = [{ statement: 'bare knuckle boxed', type: 'user' }, { statement: 'made a public statement that they do live in a box near', type: 'location' }, { statement: 'was caught mingling at a', type: 'location' }, { statement: 'Landed a role as guard number 3!', type: 'closing' }]
+    const random = Math.floor(Math.random() * (statements.length - 1))
+    return statements[random]
   }
 }
 
