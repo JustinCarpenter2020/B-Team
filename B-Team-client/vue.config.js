bdb52fc415e5a5be73e9c4bd6c78cd07527c0936
justin <JustinCarpenter2020@Gmail.com>
2021-12-03 11:41:15 -0700

    viteConfig adjusted

--- B-Team-client/vue.config.js
+++ B-Team-client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/',
-  outputDir: '../B-Team-server/client',
+  outputDir: '../B-Team-Server/client',
   productionSourceMap: false
 }
